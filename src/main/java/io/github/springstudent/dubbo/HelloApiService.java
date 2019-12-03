22c8465691caf5c23233050ef1fb53726d33218f
zhouning <2267431887@qq.com>
2019-12-03 19:38:22 +0800

    fix

--- src/main/java/io/github/springstudent/dubbo/HelloApiService.java
+++ src/main/java/io/github/springstudent/dubbo/HelloApiService.java
@@ -1,9 +1,13 @@
 package io.github.springstudent.dubbo;
 
+import java.util.List;
+
 /**
  * @author 周宁
  */
 public interface HelloApiService {
 
     String say(String message);
+
+    void print(List<String> stringList);
 }
--- src/main/java/io/github/springstudent/dubbo/HelloApiServiceImpl.java
+++ src/main/java/io/github/springstudent/dubbo/HelloApiServiceImpl.java
@@ -3,6 +3,8 @@ package io.github.springstudent.dubbo;
 import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * @author 周宁
  */
@@ -12,5 +14,14 @@ public class HelloApiServiceImpl implements HelloApiService {
     @Override
     public String say(String message) {
         return message;
+    }
+
+    @Override
+    public void print(List<String> stringList) {
+        String result="";
+        for(String str : stringList){
+            result = result + str+",";
+        }
+        System.out.println(result);
     }
 }
