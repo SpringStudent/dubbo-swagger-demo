76e6e0e1d7414d1987d8b56ecbb6b1f7934f588d
zhouning <2267431887@qq.com>
2020-02-21 10:10:41 +0800

    调整swagger骚包

--- src/main/java/io/github/springstudent/Swagger2.java
+++ src/main/java/io/github/springstudent/Swagger2.java
@@ -22,7 +22,7 @@ public class Swagger2 {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(""))
+                .apis(RequestHandlerSelectors.basePackage("io.github.springstudent"))
                 .paths(PathSelectors.any())
                 .build();
     }
